4bca74afccf91e73f2eb6f12b5dde9fdf3a92ac0
Jürgen Etzlstorfer <juergen.etzlstorfer@gmail.com>
2022-10-13 13:02:57 +0200

    use of other operator 👍

--- src/math.ts
+++ src/math.ts
@@ -6,15 +6,15 @@ export function add(x: number, y: number): number {
 
 // ### LEVEL 2: Fizz Buzz ###
 export function fizzBuzz(value: number): string {
-  if (value / 3 === 0) {
-    if (value / 5 === 0) {
+  if (value % 3 === 0) {
+    if (value % 5 === 0) {
       return "FizzBuzz";
     }
 
     return "Fizz";
   }
 
-  if (value / 5 === 0) {
+  if (value % 5 === 0) {
     return "Buzz";
   }
 
@@ -23,7 +23,7 @@ export function fizzBuzz(value: number): string {
 
 
 // ### LEVEL 3: Length of vector ###
+
 export function getLengthOfVector(vec: [number, number]): number {
-  // TODO: Return the length of the vector
-  throw new Error("Not implemented");
+  return Math.sqrt(vec[0] * vec[0] + vec[1] * vec[1]);
 }
